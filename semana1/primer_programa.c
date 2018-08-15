93f7f95fbe9fa5ad83a363e08a1a29246eb5ce5d
Sura Morales Paulina <suramp2018@dci25-186.ugto.mx>
2018-08-15 10:03:14 -0500

    estoy actualizando  Please enter the commit message for your changes. Lines starting

--- semana1/primer_programa.c
+++ semana1/primer_programa.c
@@ -6,9 +6,15 @@
 
 int main()
 {
+int edad,edad2;
 
 printf("que gran onda chiqui \n");
 printf("Es el primer programa de mi curso creado por mi\n");
+printf("Introduce tu edad: \n");
+scanf("%i",&edad);
+edad2=edad+10;
+printf("Tu edad en 2018 es : %i \n",edad);
+return 0;
 
 }
 
