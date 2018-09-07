d4e4fe88562670639b21df03a1698e768c3aa5ef
Sura Morales Paulina <suramp2018@dci25-181.ugto.mx>
2018-09-07 11:15:16 -0500

    Estoy subiendo programas

--- semana5/ejercicio4.c
+++ semana5/ejercicio4.c
@@ -1,9 +1,38 @@
 /*Programa que convierte decimal a binario, creado por Paulina Sura Morales*/
 #include<stdio.h> 
+#include<math.h>
 int main()
 {
-	int n,r[50],c;
-	int i=0;
+int residuo,decimal,div,n,r[50],c,i;
+int opcion;
+printf("Teclear una opcion\n");
+printf("(1) para convertir de binario a decimal");
+printf("(2) para covertir de decimal a binario");
+
+scanf("%i",&opcion);
+
+	
+switch(opcion)
+{
+	case 1:
+	   
+	    div=0;
+	    printf("Ingresa un numero binario:  ");
+	    scanf("%d",&n);
+	    i=0;
+	    decimal=0;
+	
+        while(n!=0){
+	     residuo=n%10;
+	     n=n/10;
+	     decimal=decimal+residuo*(pow(2,i));
+	     i++;
+                  }
+       printf("\n  Tu numero decimal es :  %d",decimal);
+
+    break;
+   
+   case 2:
 	
 	for(int k=0;k<50;k++){
 	r[k]=0;
@@ -25,7 +54,9 @@ scanf("%d",&n);
 }
 
 return 0;
-}
+}}
 		
-		
+   	
+
+
 
