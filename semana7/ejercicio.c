eaf6fedc8af4b7bcd51c79f757858a7ff761c78d
Sura Morales Paulina <suramp2018@localhost.localdomain>
2018-09-21 10:58:42 -0500

    Ejercicios semanales

--- semana7/ejercicio.c
+++ semana7/ejercicio.c
@@ -1,57 +1,42 @@
-/*Programa creado por Paulina Sura Morales*/
+/*Programa creado por Paulina Sura Morales el 21 de septiembre, el cual lee de un archivo la potencia de la funcion x a la p*/
 
-#include<stdio.h>
-#include<math.h>
-#include<stdlib.h>
-
-int main
-{
-	int p,N,nsum, i;
-	float a,b,deltax;
+#include <stdio.h>
+#include <math.h>
+int main (){
+	
 	FILE *fp;
-
-	fp=fopen("parametros.txt","r");
-	fscanf(fp,"%i %f %f %i %i",&p,&a,&b,&N,&nsum);
-	printf("%i %f %f %i %i\n",p,a,b,N,nsum);
-	fclose(fp);
-	float x[N],fx[N],intx[N];
-
-	if(p<0){
-	printf("La potencia es negativa\n");
+	float A,b,B,h,xi,xf,f,v;
+	int i,p,n;
+	A=0;
+	printf("Bienvenido, dame una potencia: \n ");
+	scanf("%d",&p);
+	if(p<=0)
+	{
+	printf("Tu numero es negativo, por favor dame otra potencia: ");
 	return 1;
-}
-	for(i=0;i<N,i++){
-		x[i]=0.;
-		fx[i]=0.;
-		intx[i]=0.;
-}
-
-	for(i=0;i<N;i++){
-	x[i]=0.;
-	fx[i]=0.;
-	intx[i]=0.;
-}
-	deltax=(b-a)/N;
-	printf("%f\n",deltax);
-	fx[i]=pow(x[i],p);
-
-	fa=pow(a,p);
-
-	for(i=0;i<=N;i++){
-	h=(x[i]-a)/nsum;
-	suma=0;
-	for(j=1;j<nsum;j++){
-	suma=suma+pow(x[i]+h*j);
-
-}
-
+	}	printf("Proporciona una x inicial : \n ");
+	scanf("%f",&xi);
+	printf("Ahora dame una x final (NOTA: mas grande que la anterior): \n ");
+	scanf("%f",&xf);
+	if(xi>xf){
+		printf("Tu x inicial resulta ser mas grande que la x final");
+		return 1;
+	}
+	printf("¿Cuantas veces se va a evaluar? \n");
+	scanf("%d",&n);
+	h=(xf-xi)/n;
+	fp=fopen("Resultados.txt","w");
+	for(i=1;i<=n;i++){
+		f=xi;
+	b=pow(f,p);
+	B=pow(f+(i*h),p);
+	A=A+((b+B)*h)/2;
+	v=h*i;
+	fprintf(fp,"%d.-\t X=%f\t F(X)=%f ^ %i\t I(X)=%f\t  \n",i,v,B,p,A);
+	b=B;
+	}
+	fclose(fp);	
+	
+	return 0;
 }
 
-fp=fopen("Resultado.txt","w");
-fprintf(fp, "x f(x) int(x)\n");
-for(i=0;i<N;i++){
-	fprintf(fp,"%f %f %f\n",x[i],fx[i],intx[i]);
-}
-fclose(fp);
-return 0;
-}
